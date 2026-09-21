40397fdc5101497a470e6d71cce5cfccad9e1162
unknown <baibouamohamed@gmail.com>
2026-09-21 16:58:12 +0200

    un programme JavaScript qui calcule et affiche la longueur d'une chaîne de caractères sans utiliser la propriété length.

--- challenge.js
+++ challenge.js
@@ -1,4 +1,5 @@
 const prompt = require("prompt-sync")();
 const lireCharachter = String(prompt("entré votre charachter or phrase :"))
+
 console.log(`la text qui vous tapi : ${lireCharachter}`)
 
---
+++ challenge2.js
@@ -0,0 +1,6 @@
+const text = "youcode"
+let compteur = 0
+for(let i = 0 ; i<text.length; i++){
+    compteur++
+}
+console.log(`la longueur d'une chaîne de caractères est : ${compteur}`)
