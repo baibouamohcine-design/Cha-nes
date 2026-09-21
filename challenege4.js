01bf7d4a2eb5fe10c4286762b9f219e8250717fa
unknown <baibouamohamed@gmail.com>
2026-09-21 17:38:43 +0200

    un programme JavaScript qui lit une chaîne de caractères et affiche la chaîne inversée

---
+++ challenege4.js
@@ -0,0 +1,9 @@
+const chaîne1 = "anas" 
+const chaîne2 = "anas"
+
+if(chaîne1===chaîne2){
+        console.log("les deux chaînes de caractères est égales")
+    }else if(console.log("les deux chaînes de caractères est non égales ")){
+    }
+
+
